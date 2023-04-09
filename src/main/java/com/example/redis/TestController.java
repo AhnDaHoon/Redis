5059efff2e649ddd4523dea63cf3ea49ed6c4f4c
DH <psycn@naver.com>
2023-04-09 22:02:11 +0900

    프로젝트 커밋

---
+++ src/main/java/com/example/redis/TestController.java
@@ -0,0 +1,34 @@
+package com.example.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+
+    // setFruit?name=banana
+
+    @GetMapping("/getFruit")
+    public String setFruit(@RequestParam String name){
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "saved.";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit(){
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String fruit = ops.get("fruit");
+
+        return fruit;
+    }
+}
