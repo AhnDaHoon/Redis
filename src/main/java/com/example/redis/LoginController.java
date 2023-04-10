1f12600c0c66770a1ba7a77bfb58993990e61e01
DH <psycn@naver.com>
2023-04-10 23:49:15 +0900

    SpringBoot에서의 세션 관리, 분산 환경의 세션 문제점

---
+++ src/main/java/com/example/redis/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.redis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@RestController
+public class LoginController {
+
+    HashMap<String, String> sessionMap = new HashMap<>();
+
+    @GetMapping("/login")
+    public String login(HttpSession session, @RequestParam String name){
+        sessionMap.put(session.getId(), name);
+
+        return "saved. ";
+    }
+
+    @GetMapping("/myName")
+    public String login(HttpSession session){
+        String myName = sessionMap.get(session.getId());
+
+        return myName;
+    }
+}
